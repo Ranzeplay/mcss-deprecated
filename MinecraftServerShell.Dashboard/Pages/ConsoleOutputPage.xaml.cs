6de83f5808099b01c27a64d53dc4f3e565d01f6f
Jeb Feng <ranzeplay@outlook.com>
2022-01-14 21:29:45 +0800

    Added some elements to MainWindow

--- MinecraftServerShell.Dashboard/Pages/ConsoleOutputPage.xaml.cs
+++ MinecraftServerShell.Dashboard/Pages/ConsoleOutputPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MinecraftServerShell.Dashboard.Pages;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MinecraftServerShell.Dashboard
+namespace MinecraftServerShell.Dashboard.Pages
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ConsoleOutputPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ConsoleOutputPage : Page
     {
-        public MainWindow()
+        public ConsoleOutputPage()
         {
             InitializeComponent();
         }
