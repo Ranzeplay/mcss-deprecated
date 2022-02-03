fc850e25a3cba08f8cf9eb3ef020687add7e0a1c
Jeb Feng <ranzeplay@outlook.com>
2022-02-03 16:18:55 +0800

    Add events to DemoPlugin

--- Plugins/Samples/DemoPlugin/Main.cs
+++ Plugins/Samples/DemoPlugin/Main.cs
@@ -1,4 +1,6 @@
-﻿using MinecraftServerShell.Core.Interfaces;
+﻿using MinecraftServerShell.Core.Events.ServerEvents.Gameplay.Player;
+using MinecraftServerShell.Core.Interfaces;
+using System.Text.Json;
 
 namespace DemoPlugin
 {
@@ -7,6 +9,10 @@ namespace DemoPlugin
         public void OnPluginLoad()
         {
             Console.WriteLine("Plugin has been loaded!");
+
+            PlayerJoinEvent.PlayerJoin += (sender, args) => Console.WriteLine("あれ？新人？　" + JsonSerializer.Serialize(args));
+            PlayerLeaveEvent.PlayerLeave += (sender, args) => Console.WriteLine("やだ。。　" + JsonSerializer.Serialize(args));
+            PlayerJoinEvent.PlayerJoin += (sender, args) => Console.WriteLine("I heard someone chatting... " + JsonSerializer.Serialize(args));
         }
 
         public void OnPluginUnload()
