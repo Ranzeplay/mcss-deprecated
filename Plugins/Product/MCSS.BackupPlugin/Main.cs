79411592b4bc2c80b89d68436d2d5695592a9ade
Jeb Feng <ranzeplay@outlook.com>
2022-02-08 22:36:19 +0800

    Create sub-project MCSS.BackupPlugin

---
+++ Plugins/Product/MCSS.BackupPlugin/Main.cs
@@ -0,0 +1,25 @@
+﻿using MCSS.BackupPlugin.Properties;
+using MinecraftServerShell.Core.Interfaces;
+using MinecraftServerShell.Core.Managers;
+
+namespace MCSS.BackupPlugin
+{
+    public class Main : IPluginEntry
+    {
+        internal static Main Instance = null!;
+
+        protected PluginLogManager LogManager = new(Resources.Name);
+
+        public void OnPluginLoad()
+        {
+            Instance = this;
+
+            LogManager.LogInfo("Plugin has been loaded!");
+        }
+
+        public void OnPluginUnload()
+        {
+            LogManager.LogInfo("Plugin has been unloaded!");
+        }
+    }
+}
