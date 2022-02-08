1d53c2da9fade58136804b0c0b301362f19bd5c0
Jeb Feng <ranzeplay@outlook.com>
2022-02-08 22:53:22 +0800

    Add default configuration value

--- MinecraftServerShell.Core/Models/AppSettings.cs
+++ MinecraftServerShell.Core/Models/AppSettings.cs
@@ -8,12 +8,21 @@ namespace MinecraftServerShell.Core.Models
 {
     public class AppSettings
     {
+        public AppSettings()
+        {
+            ServerDirectory = Path.Combine(Environment.CurrentDirectory, "Server");
+            PluginDirectory = Path.Combine(Environment.CurrentDirectory, "Plugins");
+
+            StartupCommand = string.Empty;
+            MaxLogLength = 1000;
+        }
+
         public string ServerDirectory { get; set; }
 
         public string StartupCommand { get; set; }
 
         public string PluginDirectory { get; set; }
 
-        public long MaxLogLength { get; set; } = 1000;
+        public long MaxLogLength { get; set; }
     }
 }
